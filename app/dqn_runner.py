60143f3c3725b6ae2870f0cbcc51073a7909b4ac
Phong Nguyen <phong204n@gmail.com>
2025-12-06 16:12:41 -0800

    Translated dqn_runner.py

--- app/dqn_runner.py
+++ app/dqn_runner.py
@@ -1,23 +1,120 @@
 ##  Begin Local Imports
 import model.rsaenv as rsaenv
+import model.resource as resource
 
 ##  Begin Standard Imports
-import gymnasium as gym
+# import gymnasium as gym
 import stable_baselines3 as sb3
 from stable_baselines3 import DQN
+from stable_baselines3.common import monitor, utils, env_checker
 
 ##  Example environment preset
 # env:gym.Env = gym.make("LunarLander-v3", render_mode="human")
 
 ##  Custom environment
-env:gym.Env = rsaenv.RSAEnv()
+def make_env(seed: int=100) -> monitor.Monitor:
+    env:rsaenv.RSAEnv = rsaenv.RSAEnv()
+    env = monitor.Monitor(env=env)
+    env.reset(seed=seed)
 
-model:DQN = DQN("MlpPolicy", env, verbose=1)
-model.learn(total_timesteps=10000, log_interval=4)
+##  Typical DQN config provided in template
+CONST_PROVIDED_DQN_CONFIG:dict[str:object] = {
+    "learning_rate":1e-3,
+    "buffer_size":10_000,
+    "learning_starts":100,
+    "batch_size":64,
+    "tau":0.8,
+    "gamma":0.99,
+    "train_freq":4,
+    "target_update_interval":10_000,
+    "exploration_fraction":0.2,
+    "exploration_final_eps":0.05
+}
 
-obs, info = env.reset()
-while True:
-    action, _states = model.predict(obs, deterministic=True)
-    obs, reward, terminated, truncated, info = env.step(action)
-    if terminated or truncated:
-        obs, info = env.reset()
+##  Generate a DQN model using the custom RSA Environment, then train and save it.
+def generate_and_train_rsadqn(seed:int, _debug:int=0) -> None:
+    ##  Set random seed value
+    if _debug:
+        print(f"Setting random seed to value `{str(seed)}`...")
+    utils.set_random_seed(seed)
+
+    ##  Generate custom RSA Gymnasium Environment with method `make_env`
+    if _debug:
+        print(f"Making custom RSA Gymnasium Environment...")
+        print(f"\t[Policy]:: {resource.config_values.get_option("MODEL_POLICY")}")
+        print(f"\t[Seed]:: {str(seed)}")
+        for k,v in CONST_PROVIDED_DQN_CONFIG.items():
+            print(f"\t[{k}]:: {v}")
+    env:rsaenv.RSAEnv = make_env(seed)
+    env_checker.check_env(rsaenv.RSAEnv(), warn=True)
+    model:DQN = DQN(
+            env=env, 
+            policy=str(resource.config_values.get_option("MODEL_POLICY")), 
+            seed=seed,
+            verbose=_debug + 1,
+            **CONST_PROVIDED_DQN_CONFIG
+        )
+
+    ##  Reset `env`
+    if _debug:
+        print(f"Resetting environment...")
+    env.reset()
+
+    ##  Train model
+    total_timesteps:int = 100 * int(resource.config_values.get_option("N_EPISODES"))
+    if _debug:
+        print(f"Training model...")
+        print(f"\t[Timesteps]:: {str(total_timesteps)}")
+    model.learn(total_timesteps=total_timesteps, progress_bar=True)
+
+    ##  Save model to path
+    if _debug:
+        print(f"Saving model...")
+    model.save(resource.CONST_DQN_MODEL_PATH)
+    print(f"Model saved!!  \n\t[Save Path]::{str(resource.CONST_DQN_MODEL_PATH)}.zip")
+
+    ##  Close model
+    if _debug:
+        print(f"Closing model...")
+    env.close()
+
+def test_rsadqn(seed:int, _debug:int=0) -> None:
+    ##  Setup
+    if _debug:
+        print(f"Testing model now...")
+        print(f"\t[Seed]:: {seed}")
+    test_env:rsaenv.RSAEnv = rsaenv.RSAEnv()
+    obs, info = test_env.reset(seed=seed)
+    ep_return = 0.0
+
+    ##  Load the custom trained model generated beforehand
+    if _debug:
+        print(f"Loading model from path {str(resource.CONST_DQN_MODEL_PATH)}.zip")
+    model=DQN.load(resource.CONST_DQN_MODEL_PATH)
+
+    ##  While not terminated or truncated, predict and step the model while adjusting the reward.
+    done=False
+    while not done:
+        action, _state = model.predict(obs, deterministic=True)
+        obs, reward, terminated, truncated, info = test_env.step(action)
+        if _debug:
+            print(f"\t[Reward]:: {str(reward)}")
+        ep_return += reward
+        done = terminated or truncated
+        if done:
+            print(f"Model running complete!")
+
+    if _debug:
+        print(f"Closing test environment now...")
+    test_env.close()
+
+def main() -> None:
+    CONST_DEBUG:int = int(resource.config_values.get_option("DEBUG"))
+    CONST_SEED:int = int(resource.config_values.get_option("SEED"))
+
+    generate_and_train_rsadqn(seed=CONST_SEED, _debug=CONST_DEBUG)
+
+    test_rsadqn(seed=CONST_SEED + 1, _debug=CONST_DEBUG)
+
+if __name__ == "__main__":
+    main()
---
+++ app/model/resource.py
@@ -0,0 +1,58 @@
+##  Begin Standard Imports
+import sys
+from datetime import datetime
+from configparser import ConfigParser
+from pathlib import Path
+
+CONST_ROOT_DIR:Path = Path.absolute(Path(sys.argv[0]).parent)
+# CONST_OUTPUT_DIR:Path = Path.absolute(Path.joinpath(CONST_ROOT_DIR, r".\output"))
+# CONST_OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
+
+CONST_DQN_MODEL_PATH:Path = Path.absolute(Path.joinpath(CONST_ROOT_DIR, r".\dqn_rsaenv_model"))
+
+def generateTimestamp() -> str:
+    result:str = datetime.now().strftime("%d%m%y-%H%M%S")
+    return result
+
+class configValues:
+    def __init__(self) -> None:
+        self.CONST_CONFIG_FILE:Path = Path.absolute(Path.joinpath(CONST_ROOT_DIR, r".\config.ini"))
+        self.__config_parser:ConfigParser = ConfigParser()
+
+        TMP_VALUES_:dict[str:dict[str:str]] = {
+            "DEFAULT" : {
+                "DEBUG" : "1",
+                "SEED" : "123",
+                "N_EPISODES" : "1000",
+                "MODEL_POLICY" : "MultiInputPolicy"
+            }
+        }
+
+        if Path.exists(self.CONST_CONFIG_FILE):
+            self.__config_parser.read(str(self.CONST_CONFIG_FILE))
+
+            for section,group in TMP_VALUES_.items():
+                for item,value in group.items():
+                    if not self.__config_parser.has_option(section, item)   \
+                        or self.__config_parser[section][item] == "":
+                            self.__config_parser.set(section, item, value)
+
+        else:
+            for section,group in TMP_VALUES_.items():
+                for item,value in group.items():
+                    self.__config_parser.set(section, item, value)
+
+        self.__update_config_file()
+
+    def __update_config_file(self) -> None:
+        with open(self.CONST_CONFIG_FILE, "w") as f:
+            self.__config_parser.write(f, space_around_delimiters=False)
+
+    def get_option(self, option:str, section:str="DEFAULT") -> str:
+        return self.__config_parser[section][option]
+    
+    def set_option(self, target:str, option:str, section:str="DEFAULT") -> str:
+        self.__config_parser.set(section, option, str(target))
+        self.__update_config_file()
+
+config_values = configValues()
